384563058eeb643b9ba07df35e5fde836d029311
Satvik Sashwat <satviksashwat321@gmail.com>
2026-08-19 09:44:17 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+int main()
+{
+int number1,number2,sum;
+printf ("ENTER 2 NUMBERS");
+scanf("%d %d",&number1,&number2);
+
+sum=number1+number2;
+printf("sum of your numbers is: %d" , sum);
+
+
+
+
+}
